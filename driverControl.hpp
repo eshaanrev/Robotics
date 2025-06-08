396fc621b8df9f71eda8fc8ae888cfdcf93ba40f
Eshaan <105980431+0825eshaan@users.noreply.github.com>
2025-06-07 23:26:50 -0400

    Add files via upload

---
+++ driverControl.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+void drive();
+void intake_toggle();
+void clamp_toggle();
+void lb_cycle();
---
+++ globals.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "lemlib/api.hpp"
+#include "pros/misc.hpp"
+#include "pros/motor_group.hpp"
+#include "pros/motors.hpp"
+#include "pros/rtos.hpp"
+
+// Controller
+extern pros::Controller controller;
+
+// Drive Motors
+extern pros::MotorGroup left_motors;
+extern pros::MotorGroup right_motors;
+extern pros::Motor intake_motor;
+extern pros::Motor intake_motor;
+
+// LemLib
+extern lemlib::Drivetrain drivetrain;
+extern lemlib::OdomSensors sensors;
+extern lemlib::ControllerSettings lateral_controller;
+extern lemlib::ControllerSettings angular_controller;
+extern lemlib::Chassis chassis;
+extern pros::adi::Pneumatics clamp;
+
+// Clamp
+extern bool clampClosed;
+extern bool buttonPressed;
+
+// Ring Grabber
+extern pros::Motor lb_motor;
+extern int lb_position; // 0=low, 1=medium, 2=high
+extern bool lb_pressed;
